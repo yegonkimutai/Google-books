1566822e6e7e80025f4ca4221f71aec21f466c82
Brian Yrgon <yegonkimutai.99@gmail.com>
2023-06-01 16:38:22 +0300

    :construction:Create class component

---
+++ src/components/Card.js
@@ -0,0 +1,15 @@
+const Card = () => {
+    return(
+        <>
+        <div className='card'>
+            <img src='./images/book.jpg' alt=''/>
+            <div className='bottom'>
+                <h3 className='title'>React Js</h3>
+                <p className='amount'>&#8377;3290</p>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Card
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,18 @@
+import { useState } from "react"
 import './style.css'
+import Card from './Card'
+import axios from 'axios'
 
 const Main = () => {
+    const [search, setSearch] = useState('');
+    const searchBook = (e) => {
+        if(e.key === 'Enter') {
+            axios.get('https://www.googleapis.com/books/v1/volumes?q='+search+'&key=AIzaSyAtl6Pxq-ulSyusBAvFkcEdlPAfcfdrW_s')
+            .then(res => console.log(res.data.items))
+            .catch(err => console.log(err))
+        }
+    }
+
     return(
         <>
         <div className='header'>
@@ -10,11 +22,28 @@ const Main = () => {
             <div className='row2'>
                 <h2>Find Your Book</h2>
                 <div className='search'>
-                    <input type='text' placeholder='Enter book name'/>
-                    <button><i class="fa-solid fa-magnifying-glass"></i></button>
+                    <input
+                     type='text'
+                    placeholder='Enter book name' 
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
+                    onKeyDown={searchBook}
+                    />
+                    <button><i className="fa-solid fa-magnifying-glass"></i></button>
                 </div>
                 <img src='./images/bg2.png'/>
             </div>
+        </div>
+
+        <div className='container'>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
         </div>
         </>
     )
